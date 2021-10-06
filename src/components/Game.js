33c1a6fc412c591d2f4d62222e109a4fcc5dd950
nhanlinh1478 <luongtrongnhan1082000@gmail.com>
2021-10-06 20:28:21 +0700

    update02

--- src/components/Game.js
+++ src/components/Game.js
@@ -119,7 +119,7 @@ function Game() {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    for (let i = 0; i < newBoard; i++) {
+    for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (
         squares[a] &&
